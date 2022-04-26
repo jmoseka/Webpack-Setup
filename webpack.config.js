0c417c9620ae325c81853e1e4c885451452a05c6
jmoseka <jamylam3@gmail.com>
2022-04-26 14:43:46 +0300

    add webconfig files

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
